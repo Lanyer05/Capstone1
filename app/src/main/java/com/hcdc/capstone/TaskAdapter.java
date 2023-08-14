f5354b125683acca476150dc715993a4b595c5eb
runemarcx <aaronmarc.padilla@hcdc.edu.ph>
2023-08-14 15:59:29 +0800

    new fetsors added v2

--- app/src/main/java/com/hcdc/capstone/TaskAdapter.java
+++ app/src/main/java/com/hcdc/capstone/TaskAdapter.java
@@ -1,11 +1,13 @@
 package com.hcdc.capstone;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -43,6 +45,15 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder
         Log.d("TaskAdapter", "Binding task: " + tasks.getPoints());
         Log.d("TaskAdapter", "Binding task: " + tasks.getLocation());
         Log.d("TaskAdapter", "Binding task: " + tasks.getDescription());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(context.getApplicationContext(), "Item clickerd", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
     @Override
