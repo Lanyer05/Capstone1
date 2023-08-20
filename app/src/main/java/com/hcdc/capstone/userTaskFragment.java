6a38b82b8c1bc7d8e10970c0b7692a09cf16df57
Hi <nobelaodlanyer@gmail.com>
2023-08-20 14:21:20 +0800

    Initial Commit

--- app/src/main/java/com/hcdc/capstone/userTaskFragment.java
+++ app/src/main/java/com/hcdc/capstone/userTaskFragment.java
@@ -1,12 +1,25 @@
 package com.hcdc.capstone;
+
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class userTaskFragment extends Fragment {
+
+    private FirebaseAuth auth;
+    private FirebaseFirestore firestore;
+    private TextView taskNameTextView, taskPointsTextView, taskLocationTextView, taskTimeFrameTextView, taskDescriptionTextView;
 
     public userTaskFragment() {
         // Required empty public constructor
@@ -15,7 +28,55 @@ public class userTaskFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.usertask_fragment, container, false);
+        View view = inflater.inflate(R.layout.usertask_fragment, container, false);
+
+        auth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
+
+        taskNameTextView = view.findViewById(R.id.taskTitle);
+        taskPointsTextView = view.findViewById(R.id.taskPoint);
+        taskLocationTextView = view.findViewById(R.id.taskLocation);
+        taskTimeFrameTextView = view.findViewById(R.id.taskTimeFrame);
+        taskDescriptionTextView = view.findViewById(R.id.taskDesc);
+
+        String currentUserUID = auth.getCurrentUser().getUid();
+
+        firestore.collection("user_acceptedTask")
+                .whereEqualTo("acceptedBy", currentUserUID)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        QuerySnapshot documents = task.getResult();
+                        if (documents != null && !documents.isEmpty()) {
+                            // Assuming there's only one accepted task per user
+                            QueryDocumentSnapshot document = (QueryDocumentSnapshot) documents.getDocuments().get(0);
+
+                            String taskName = document.getString("taskName");
+                            String taskPoints = document.getString("points");
+                            String taskLocation = document.getString("location");
+                            String taskDescription = document.getString("description");
+
+                            // Handle time frame
+                            Long hoursLong = document.getLong("hours");
+                            Long minutesLong = document.getLong("minutes");
+
+                            int taskHours = hoursLong != null ? hoursLong.intValue() : 0;
+                            int taskMinutes = minutesLong != null ? minutesLong.intValue() : 0;
+
+                            String taskTimeFrame = "";
+                            if (taskHours > 0 || taskMinutes > 0) {
+                                taskTimeFrame = "Time Frame: " + taskHours + " hours " + taskMinutes + " minutes";
+                            }
+
+                            taskNameTextView.setText(taskName);
+                            taskPointsTextView.setText(taskPoints);
+                            taskLocationTextView.setText(taskLocation);
+                            taskTimeFrameTextView.setText(taskTimeFrame);
+                            taskDescriptionTextView.setText(taskDescription);
+                        }
+                    }
+                });
+
+        return view;
     }
 }
